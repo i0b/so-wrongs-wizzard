e36eb7dd2e9441559b7ce86e73dfb93430fe047c
Surong Li <surong.li@uni-ulm.de>
2019-01-26 15:34:39 +0100

    init

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour
+{
+    public delegate void GameDelegate();
+    public static event GameDelegate OnGameStarted;
+    public static event GameDelegate OnGameEnded;
+
+
+    public static GameManager Instance;
+
+    enum GameState
+    {
+        None,
+        Countdown,
+        Started,
+        EnterHighscore,
+        GameOver,
+        Highscore,
+        Help
+    }
+
+    int score = 0;
+    bool gameOver = false;
+
+    public bool GameOver { get { return gameOver; } }
+
+    private void Awake()
+    {
+        Instance = this;
+    }
+
+    void SetGameState(GameState state)
+    {
+        switch(state)
+        {
+            case GameState.None:
+                break;
+
+            case GameState.Countdown:
+                break;
+
+            case GameState.Started:
+                break;
+
+            case GameState.EnterHighscore:
+                break;
+
+            case GameState.GameOver:
+                break;
+
+            case GameState.Highscore:
+                break;
+
+            case GameState.Help:
+                break;
+        }
+    }
+
+    public void ConfirmGameOver() {
+        OnGameEnded();
+    }
+
+    public void StartGame() {
+        OnGameStarted();
+    }
+}
---
+++ Assets/Scripts/TapController.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TapController : MonoBehaviour
+{
+    public float tapForce = 300;
+    public float tiltSmooth = 5;
+    //public Vector3 startPosition;
+
+    Rigidbody2D rigidbody;
+    Quaternion downRotation;
+    Quaternion forwardRotation;
+
+
+    void Start()
+    {
+        rigidbody = GetComponent<Rigidbody2D>();
+        downRotation = Quaternion.Euler(0, 0, -50);
+        forwardRotation = Quaternion.Euler(0, 0, 20);
+        // rigidbody.simulated = false;
+    }
+
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            transform.rotation = forwardRotation;
+            rigidbody.velocity = Vector3.zero;
+            rigidbody.AddForce(Vector3.up * tapForce, ForceMode2D.Force);
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        transform.rotation = Quaternion.Lerp(transform.rotation, downRotation, tiltSmooth * Time.deltaTime);
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "ScoreZone") { 
+            // score update
+        }
+        if(collision.gameObject.tag == "DeadZone") {
+            rigidbody.simulated = false;
+        }
+    }
+}
